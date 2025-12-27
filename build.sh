c14666827eb5c2ef5f47dbf491d88c92a9c089cf
Jāgaro <jagaro@dayam.org>
2025-12-27 14:19:49 +0000

    t

--- build.sh
+++ build.sh
@@ -3,19 +3,21 @@
 set -eu
 cd "$(dirname "$0")"
 
-export OPENWRT_REVISION="v25.12.0-rc1"
-export OPENWRT_TARGETS="lantiq-xrx200"
+OPENWRT_REVISIONS="v24.10.5 v25.12.0-rc1"
+OPENWRT_TARGETS="lantiq-xrx200 ramips-mt7621 x86-64"
 
+export OPENWRT_REVISIONS
 export OPENWRT_TARGET
 
 ./1-prerequisites.sh # needs sudo
-./2-clone.sh
 
-for OPENWRT_TARGET in $OPENWRT_TARGETS; do
-  make dirclean
-  ./3-config.sh
-  ./4-download.sh
-  ./5-toolchain.sh
-  ./6-compile.sh
-  ./7-release.sh
+for OPENWRT_REVISION in $OPENWRT_REVISIONS; do
+  for OPENWRT_TARGET in $OPENWRT_TARGETS; do
+    ./2-clone.sh
+    ./3-config.sh
+    ./4-download.sh
+    ./5-toolchain.sh
+    ./6-compile.sh
+    ./7-release.sh
+  done
 done
