84c38c2da6cc04bd44ac9e63833e2228f6195ff6
Jāgaro <jagaro@dayam.org>
2025-12-27 09:26:29 +0000

    6

--- 7-compile.sh
+++ 7-compile.sh
@@ -3,4 +3,5 @@
 set -eu
 
 cd openwrt
-make -j $(($(nproc)+1)) V=s package/network/services/dnsmasq/compile
+make -j $(($(nproc)+1)) V=s world
+#make -j $(($(nproc)+1)) V=s package/network/services/dnsmasq/compile
