60e60e0e579c4b91b5f2366452f8d131555b1171
Jāgaro <jagaro@dayam.org>
2025-12-27 14:50:04 +0000

    more

--- 7-release.sh
+++ 7-release.sh
@@ -9,7 +9,7 @@ ARCH="$(ls -1 openwrt/bin/packages)"
 for APK_PATH in "openwrt/bin/packages/$ARCH/base/dnsmasq-"*.apk; do
   RELEASE_NAME="$(
     basename "$APK_PATH" |
-      sed 's/^\(.*\)-\([2-9]\.[0-9].*\)$/\1-'"$ARCH"'-\2/'
+      sed 's/^\(.*\)-\([2-9]\.[0-9].*\)$/\1-'"${OPENWRT_REVISION}-${ARCH}"'-\2/'
   )"
   cp "$APK_PATH" "release/$RELEASE_NAME"
 done
