35ba0aca1af7f5f58bdb5f99288be78dbabc6aec
Jāgaro <jagaro@dayam.org>
2025-12-28 05:15:41 +0000

    fix package names

--- 7-release.sh
+++ 7-release.sh
@@ -6,10 +6,16 @@ mkdir -p release
 
 ARCH="$(ls -1 openwrt/bin/packages)"
 
-for PACKAGE_PATH in "openwrt/bin/packages/$ARCH/base/dnsmasq-"*; do
-  RELEASE_NAME="$(
-    basename "$PACKAGE_PATH" |
-      sed 's/^\(.*\)-\([2-9]\.[0-9].*\)$/\1-'"${OPENWRT_REVISION}-${ARCH}"'-\2/'
-  )"
-  cp "$PACKAGE_PATH" "release/$RELEASE_NAME"
+for PACKAGE_PATH in "openwrt/bin/packages/$ARCH/base/dnsmasq"*; do
+  PACKAGE_FILE="$(basename -- "$PACKAGE_PATH")"
+  PACKAGE_EXT="${PACKAGE_FILE##*.}"
+  if [ "$PACKAGE_EXT" = apk ]; then
+    PACKAGE_NAME="$(echo "$PACKAGE_FILE" | sed -e 's/-[0-9].*\.apk$//')"
+    PACKAGE_VERSION="$(echo "$PACKAGE_FILE" | sed -e 's/^.*-\([0-9].*\)\.apk$/\1/')"
+  else
+    PACKAGE_NAME="$(echo "$PACKAGE_FILE" | cut -d _ -f 1)"
+    PACKAGE_VERSION="$(echo "$PACKAGE_FILE" | sed -e 's/^[^_]*_//' -e 's/_[^_]*_[^_]*\.ipk$//')"
+  fi
+  RELEASE_FILE="$PACKAGE_NAME-$PACKAGE_VERSION-openwrt-$OPENWRT_REVISION-$ARCH.$PACKAGE_EXT"
+  echo cp "$PACKAGE_PATH" "release/$RELEASE_FILE"
 done
